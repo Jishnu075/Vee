05c33ec5ee6f29eb38bd9ff5bbed0fef86d62bf8
Jishnu075 <jishnubiju075@gmail.com>
2021-05-18 16:58:28 +0530

    retrieval of events data from db to html file

--- vjc/views.py
+++ vjc/views.py
@@ -1,20 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Events
 
 def home(request):
-	# return HttpResponse('<h1>HOME</h1>')
+	
+	context = {
+		'events': Events.objects.all()
+	}
 
-	return render(request, 'vjc/tmp.html')
+
+	return render(request, 'vjc/tmp.html', context)
+
 
 def about(request):
 	return HttpResponse('Sample about page')
-	# context = {
-	# 	"first_name" : "Axew",
-	# }
-	# context['first_name'] =  "Axew"
-	# context['mid_name'] =  "Axew 1"
-	# context['last_name'] =  "Axew 2"
-	# return render(request, 'vjc/about.html',context)
+	return render(request, 'vjc/about.html')
+
+
+
 
 # def log_reg (request):
 	
