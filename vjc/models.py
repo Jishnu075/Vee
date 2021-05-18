2e3d4d6b3286304dbe086b7e2c50f200a663cc1b
Jishnu075 <jishnubiju075@gmail.com>
2021-05-18 16:35:10 +0530

    added events class

--- vjc/models.py
+++ vjc/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Events(models.Model):
+    title = models.CharField(max_length=200)
+    about_event = models.TextField()
+    date_posted = models.DateTimeField(auto_now_add=True)
+    organiser = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+
+
+
+
+
