ef64bf41245052ab8a81c30f67a86cb808886dca
Jishnu075 <jishnubiju075@gmail.com>
2021-05-25 02:36:21 +0530

    updated static root

--- vee/settings.py
+++ vee/settings.py
@@ -135,8 +135,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 STATICFILES_DIRS = [
     BASE_DIR / "static",
-]
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
